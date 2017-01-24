0774a13c51a37cd1777355d397750bfbd8a3a078
Zaid-Ajaj <zaid.naom@gmail.com>
2017-01-25 00:12:02 +0100

    Initial Cable.Client + Gzip compression

---
+++ Cable.Client/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cable.Client
+{
+    public class Client
+    {
+        public static T Resolve<T>()
+        {
+            return default(T);
+        }
+    }
+}
---
+++ Cable/Gzip.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cable
+{
+    public static class Gzip
+    {
+        /// <summary>
+        /// Compresses a string into a base64 string using gzip compression
+        /// </summary>
+        /// <param name="text">the string to compress</param>
+        /// <returns>string</returns>
+        public static string Compress(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            MemoryStream ms = new MemoryStream();
+            using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+            {
+                zip.Write(buffer, 0, buffer.Length);
+            }
+
+            ms.Position = 0;
+            MemoryStream outStream = new MemoryStream();
+
+            byte[] compressed = new byte[ms.Length];
+            ms.Read(compressed, 0, compressed.Length);
+
+            byte[] gzBuffer = new byte[compressed.Length + 4];
+            Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+            Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+            return Convert.ToBase64String(gzBuffer);
+        }
+
+        /// <summary>
+        /// Decompresses the gzipped-base64 compressed string and returns the original string
+        /// </summary>
+        /// <param name="compressedText">the compressed string</param>
+        /// <returns>string</returns>
+        public static string Decompress(string compressedText)
+        {
+            if (string.IsNullOrEmpty(compressedText))
+                return "";
+
+            byte[] gzBuffer = Convert.FromBase64String(compressedText);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int msgLength = BitConverter.ToInt32(gzBuffer, 0);
+                ms.Write(gzBuffer, 4, gzBuffer.Length - 4);
+
+                byte[] buffer = new byte[msgLength];
+
+                ms.Position = 0;
+                using (GZipStream zip = new GZipStream(ms, CompressionMode.Decompress))
+                {
+                    zip.Read(buffer, 0, buffer.Length);
+                }
+
+                return Encoding.UTF8.GetString(buffer);
+            }
+        }
+    }
+}
